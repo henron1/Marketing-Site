6a06a2258c45ca2afc6d837dd00f99b37870124d
henron1 <40742091+henron1@users.noreply.github.com>
2024-05-04 15:31:10 -0400

    hero-section: feat: update slim layout gradient w/ brand colors, add gradient and waves files and add cta buttons for hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,6 +24,20 @@ export function Hero() {
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
         Learn from the largest variety of musicians across all genres in the musical universe
       </p>
+      <div className="mt-10 flex justify-center gap-x-6">
+        <Button href="/register">Get 6 months free</Button>
+        {/* need to add register page */}
+        <Button href="" variant="outline">
+          {/* href will go to promo youtube video */}
+          <svg
+            aria-hidden="true"
+            className="h-3 w-3 flex-none fill-brand-forest-500 group-active:fill-current"
+          >
+            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
+          </svg>
+          <span className="ml-3">Watch video</span>
+        </Button>
+      </div>
     </Container>
   );
 }
